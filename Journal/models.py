791acd05e920e8c1fcaa34bf8db8e9f088201a35
Pawel <pawel.wolinski@op.pl>
2018-02-18 19:05:17 +0100

    Project repo

---
+++ Journal/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    questionText = models.CharField(max_length = 200)
+    publishDate = models.DateTimeField("Question publish date", null = True)
+
+class Choice(models.Model):
+    question = models.ForeignKey("Question", on_delete=models.DO_NOTHING)
+    answer = models.CharField(max_length = 200)
+    votesCount = models.IntegerField(default=0)
+
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    postText = models.TextField()
+    publishDate = models.DateTimeField(default = timezone.now)
+    author = models.CharField(max_length = 35)
+
