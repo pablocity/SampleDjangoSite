832cd57a349da54a338f400caee4a1fb4db386e2
Pawel <pawel.wolinski@op.pl>
2018-02-18 21:27:37 +0100

    Created simple View which renders sample html page

--- Journal/admin.py
+++ Journal/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from .models import Question
 from .models import Choice
+from django.contrib import admin
 from .models import Post
 
-#admin.site.register(Question)
 admin.site.register(Post)
 # Register your models here.
+#admin.site.register(Question)
